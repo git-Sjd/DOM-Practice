0cf8a673c60133b46c0b1df38300c1d851194531
git-Sjd <sajid.tisl@gmail.com>
2022-12-24 01:35:21 +0530

    DOM

---
+++ script.js
@@ -0,0 +1,21 @@
+var navBar = document.getElementById("navBar");
+console.log(navBar)
+
+var para = document.getElementsByClassName("Para")
+console.log(para)
+
+var tag = document.getElementsByTagName("header")
+console.log(tag)
+
+// if(para.length === 2){
+//     alert("There are five components in header")
+// }
+// else{
+//     alert("Hello")
+// }
+
+var para1 = document.querySelector('.Para p')
+console.log(para1)
+
+var para2 = document.querySelectorAll(".Para p")
+console.log(para2)
